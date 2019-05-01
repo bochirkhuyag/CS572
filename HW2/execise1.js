48d1db4ab1583e78eaf9ed6e524cf55369a85048
bochirkhuyag <49167544+bochirkhuyag@users.noreply.github.com>
2019-05-01 14:39:56 -0500

    exercise 1 finished

--- HW2/execise1.js
+++ HW2/execise1.js
@@ -37,3 +37,24 @@ console.log("start");
 [1,2,3,4,5,6,7,8].even();
 [1,2,3,4,5,6,7,8].odd();
 console.log("end");
+
+/*
+1.order of execution between setImmediate(callback) and setTimout(callback, 0)
+is varying depending on performance and which phase event loop is executing.
+However inside I/O operation setImeddiate always runs first because setImmediate runs at
+check phase which is after poll.
+2. process.nextTick is runs before any other phases it's not actually part of event loop it's c++ handler
+which is checked after every phase of event loop.
+3. Node built in modules
+stream - handle streaming data
+fs - work with files
+http -to make node.js act as https server
+https - to make node.js act as https server
+zlib - compress or decompress file
+util - access utility functions
+events - handle events
+buffer - work with binary data
+net - create server and client
+path - handle file path
+
+ */
